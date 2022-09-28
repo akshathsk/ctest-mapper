e8cd2199dcfe72d0cd1af6352d0fb1b810096f06
akshathsk <akshath.sk@gmail.com>
2022-09-27 19:14:02 -0500

    initial commit

---
+++ src/main/java/org/uiuc/Main.java
@@ -0,0 +1,46 @@
+package org.uiuc;
+
+import org.apache.maven.shared.invoker.*;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    private static final List<String> PUBLISH_GOALS = Arrays.asList("clean", "site-deploy");
+    private final Invoker invoker;
+
+    public Main() {
+        this.invoker = new DefaultInvoker();
+    }
+
+    public Main(File localRepositoryDir) {
+        Invoker newInvoker = new DefaultInvoker();
+        newInvoker.setLocalRepositoryDirectory(localRepositoryDir);
+
+        this.invoker = newInvoker;
+    }
+
+    public void publishSite(File siteDirectory) throws Exception {
+        InvocationRequest request = new DefaultInvocationRequest();
+        request.setPomFile(new File("/home/sk117/final-project/apache-skywalking-apm-9.2.0/oap-server/server-starter/pom.xml"));
+        request.setGoals(PUBLISH_GOALS);
+        InvocationResult result = invoker.execute(request);
+        System.out.println(result);
+        if (result.getExitCode() != 0) {
+            if (result.getExecutionException() != null) {
+                throw new Exception("Failed to publish site." +
+                        result.getExecutionException());
+            } else {
+                throw new Exception("Failed to publish site. Exit code: " +
+                        result.getExitCode());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+    }
+}
